f654c43c1b72166073709c1136ba68d9d3e2e5cd
martindurantdf <104039754+martindurantdf@users.noreply.github.com>
2022-05-26 22:51:15 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+/**
+ • Diseñar un algoritmo que recorra las
+butacas de una sala de cine y determine
+cuántas butacas desocupadas hay
+• Suponga que para modelar este
+problema, se utiliza un arreglo con
+valores lógicos
+• La presencia de un valor verdadero (true) en
+el arreglo indica que la butaca está ocupada
+• La presencia de un valor falso (false) en el
+arreglo indica que la butaca está desocupada
+ */
+
+let longitud: number = Number(prompt("Ingrese la cantidad de butacas del cine:"));
+let salaDeCine: boolean[] = new Array(longitud);
+
+//Funcion para generar aleratoriamente butacas libres y ocuapadas
+function cargarSalaAleatoria(arraySala: boolean[]) {
+  for (var i = 0; i < arraySala.length; i++) {
+    arraySala[i] = Math.random() < 0.5;
+  }
+}
+//
+//Funcion para buscar contar las butacas ocupadas
+function buscarButacasDesocupadas(arraySala: boolean[]): number {
+  let desocupadas: number = 0;
+  for (var index in arraySala) {
+    if (arraySala[index] === false) {
+      desocupadas++;
+    }
+  }
+  return desocupadas;
+}
+console.log("Se generarán automaticamente las butacas libres y ocupadas");
+cargarSalaAleatoria(salaDeCine);
+console.log(salaDeCine);
+console.log("Butacas desocupadas:", buscarButacasDesocupadas(salaDeCine));
